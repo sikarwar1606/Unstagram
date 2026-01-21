cd96ae4554affda43174f2f9cd689539b95e115f
Anupam Singh Sikarwar <sikarwar1606@gmail.com>
2026-01-21 18:53:22 +0530

    Added one sample video on reel page and modified the other component like add feature to pop the like button when click

--- src/Components/Reels/Reels.jsx
+++ src/Components/Reels/Reels.jsx
@@ -1,32 +1,50 @@
 import React from 'react'
+import { useState } from 'react'
+import {Heart} from 'lucide-react'
+
 
 const Reels = () => {
+
+  const [isLiked, setisLiked] = useState(false)
+
+  const like = ()=>{
+    console.log("Liked");
+    
+  }
   return (
-    <div className='flex relative h-screen' >
-      <div className="top flex gap-25 px-5 py-2">
+    <div className=' relative h-screen w-full overflow-hidden ' >
+      <video className='h-full w-full object-cover ' playsInline controls muted autoPlay loop src="/video.mp4">
+
+      </video>
+      <div className="absolute  top-0 left-0 w-full flex gap-30 items-center px-4 py-3 z-10">
         <div>+</div>
-        <div className='flex gap-5'>
+        <div className='flex gap-5 font-semibold'>
             <p>Reels</p>
-            <p>Friends</p>
+            <p className='opacity-70'>Friends</p>
         </div>
       </div>
 
-      <div className="side absolute right-0 flex flex-col items-center justify-between top-[50%] h-50">
-        <div>Like</div>
-        <div>Comment</div>
-        <div>Share</div>
+      <div className="side absolute right-3 bottom-24 flex flex-col items-center gap-6 z-10 ">
+        <div className='flex flex-col items-center'>
+          <button onClick={()=> setisLiked(!isLiked)} className='text-3xl active:scale-90 transition'>
+            <Heart className={isLiked ? "stroke-red-500 fill-red-500" : "text-transparent stroke-white"} />
+            </button>
+        </div>
+        <div>🗨️</div>
+        <div>🦈</div>
         <div>:</div>
       </div>
 
-      <div className="absolute bottom-0 flex flex-col">
-        <div className='flex'>
-            <div>Profile</div>
-            <div>Follow</div>
+      <div className="absolute bottom-10 left-3 z-10">
+        <div className='flex items-center gap-3'>
+            <div className='w-8 h-8 rounded-full bg-gray-400'>             
+            </div>
+            <p className='font-semibold'>@username</p>
+            <button className='border px-2 py-1 rounded text-xs'>
+              Follow
+            </button>
         </div>
-        <div className='flex'>
-            <div><p>See more</p></div>
-            <div>:</div>
-        </div>
+        <p className='text-sm mt-2'>This is reel caption... <span className='opacity-70'>more</span></p>
       </div>
     </div>
   )
